ec90e611fc69c53c985cfdfa128145029cc258d3
resource777 <79133019+resource777@users.noreply.github.com>
2022-03-13 19:54:30 +0900

    Update server.c

--- server.c
+++ server.c
@@ -9,49 +9,41 @@
 /*   Updated: 2022/03/06 19:43:05 by jaewonki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "minitalk.h"
-
-void	binary_to_decimal(int binary, int pid)
-{
-	static unsigned char	ch = 0;
-	static int				index = 7;
-
-	ch += (binary << index--);
-	if (index < 0)
-	{
-		write(1, &ch, 1);
-		index = 7;
-		ch = 0;
-		if (kill(pid, SIGUSR1) == -1)
-		{
-			exit(0);
-		}
-		usleep(100);
-	}
-}
-
-void	serverhandler(int signum, siginfo_t *siginfo, void *context)
-{
-	(void)siginfo;
-	(void)context;
-	binary_to_decimal(signum == SIGUSR2, siginfo->si_pid);
-}
 
 int	main(void)
 {
-	struct sigaction	catch;
+	pid_t	serverPID;
 
-	ft_putstr_fd("Server PID : ", 1);
-	ft_putnbr_fd(getpid(), 1);
-	write(1, "\n", 2);
-	catch.sa_flags = SA_SIGINFO;
-	catch.sa_sigaction = serverhandler;
-	if (sigaction(SIGUSR1, &catch, NULL) == -1)
-		exit(1);
-	if (sigaction(SIGUSR2, &catch, NULL) == -1)
-		exit(1);
+	serverPID = getpid();
+	if (serverPID)
+	{
+		ft_putnbr_fd(serverPID, 1);
+		ft_putstr_fd("\n", 1);
+	}
+	signal(SIGUSR1, signalHandler);
+	signal(SIGUSR2, signalHandler);
 	while (1)
 		pause();
 	return (0);
+}
+
+void	signalHandler(int sigNum)
+{
+	static char	receive = 0;
+	static int	idx = 0;
+
+	receive <<= 1;
+	if (sigNum == SIGUSR1)
+		receive += 0;
+	else
+		receive += 1;
+	idx++;
+	if (idx == 8)
+	{
+		ft_putchar_fd(receive, 1);
+		receive = 0;
+		idx = 0;
+	}
+	return ;
 }
